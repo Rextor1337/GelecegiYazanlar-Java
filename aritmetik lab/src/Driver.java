fb1f8e2a87f3f6bdff79425df3fa60b52d702d48
Rextor1337 <onur.resat@mail.ru>
2022-07-16 00:43:36 +0300

    .

---
+++ aritmetik lab/src/Driver.java
@@ -0,0 +1,18 @@
+
+public class Driver {
+	public static void main(String[] args) {
+		int i = 10;
+		int x = 4;
+		
+		System.out.println(~i);
+		System.out.println(i & x); // and operatoru gibi dusun 0 & 1= 0
+		
+		System.out.println( i|x ); //ayni mantik or
+		
+		System.out.println( i >> 1); // yariya boler
+		System.out.println(i << 1); // iki katina cikarir //binary kodda degerleri kaydiriyor
+		
+		
+	}
+
+}
